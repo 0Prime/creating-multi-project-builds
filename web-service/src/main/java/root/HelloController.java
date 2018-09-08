e27f35678a9256fc5835b588897eac4c74c01c35
O_Prime <sel-egor@yandex.ru>
2018-09-08 14:42:26 +0300

    Added simple dependency into web-service

--- web-service/src/main/java/root/HelloController.java
+++ web-service/src/main/java/root/HelloController.java
@@ -1,7 +1,11 @@
 package root;
 
 import greeter.GreetingFormatter;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import root.config.FooService;
 
 
 @RestController
@@ -9,6 +13,14 @@ public class HelloController {
 
 	@GetMapping("/hello")
 	public String greet(@RequestParam(defaultValue = "World", name = "name") String name) {
+		service.doWork();
 		return GreetingFormatter.greeting(name);
 	}
+
+
+	@Autowired
+	public HelloController(FooService service) {this.service = service;}
+
+
+	private final FooService service;
 }
