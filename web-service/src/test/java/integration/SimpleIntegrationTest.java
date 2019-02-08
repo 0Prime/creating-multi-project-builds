ae71f0848f502dc77f581964524faf57429411ab
NARI\e.selyanin <sel-egor@yandex.ru>
2019-02-08 17:45:53 +0300

    add HelloController MockMvc tests

--- web-service/src/test/java/integration/SimpleIntegrationTest.java
+++ web-service/src/test/java/integration/SimpleIntegrationTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import root.config.*;
 
 import static org.junit.Assert.assertNotNull;
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {RootConfig.class, FooServiceImp.class})
